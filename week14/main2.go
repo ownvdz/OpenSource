f3b1c513ce7a95547200362ebbabb757884674cb
ownvdz <128668961+ownvdz@users.noreply.github.com>
2023-11-27 17:27:19 +0900

    Add files via upload

---
+++ week14/main2.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var games map[int]string
+	// games = make(map[int]string)
+	// games := map[int]string{456: "성기훈", 218: "박해수", 067: "강새벽", 001: "오일남", 199: "알리", 101: "아이오아이"}
+	games := map[int]string{
+		456: "성기훈",
+		218: "박해수",
+		067: "강새벽",
+		001: "오일남",
+		199: "알리",
+		101: "아이오아이",
+	}
+	// // append
+	// games[456] = "성기훈"
+	// games[218] = "박해수"
+	// games[067] = "강새벽"
+	// games[001] = "오일남"
+	// games[199] = "알리"
+	// games[101] = "아이오아이"
+
+	// fmt.Println(games[067])
+	for _, v := range games {
+		fmt.Println(v)
+	}
+
+	// update
+	games[101] = "장덕수"
+	// delete
+	delete(games, 199)
+
+	for k, v := range games {
+		fmt.Println(k, v)
+	}
+}
